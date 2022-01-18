92b37f7fc11bb99ed68355f5b34cf81155e51d08
Yousuf Minhaj <yousufminhajym@gmail.com>
2022-01-18 11:56:18 +0500

    Final Commit

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
+import seaborn as sns
+sns.set()
+from sklearn import metrics
+from pprint import pprint
+from datetime import datetime
+import collections
+import re
 import math, random
 from datetime import datetime
 import datetime as dt
@@ -24,8 +31,12 @@ from textblob import TextBlob
 import constants as ct
 from Tweet import Tweet
 import nltk
+nltk.download('stopwords')
 nltk.download('punkt')
-
+nltk.download('wordnet')
+from nltk.corpus import wordnet, stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 # Ignore Warnings
 import warnings
 warnings.filterwarnings("ignore")
@@ -45,7 +56,30 @@ def add_header(response):
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    df = pd.read_csv('ticker.csv')
+    df.set_index('Ticker')
+    ticker_list = []
+    ticker_date = []
+    ticker_open = []
+    ticker_high = []
+    ticker_low = []
+    ticker_close = []
+    ticker_volume = []
+    print(df.head())
+    x = dt.datetime.now()
+    for index, row in df.iterrows():
+        #print(row['Ticker'])
+        data = yf.download(row['Ticker'], start=x)
+        dff = pd.DataFrame(data=data)
+        ticker_list.append(row['Ticker'])
+        for index1, row1 in dff.iterrows():
+            ticker_date.append(x)
+            ticker_open.append(round(row1['Open'],2))
+            ticker_high.append(round(row1['High'],2))
+            ticker_low.append(round(row1['Low'],2))
+            ticker_close.append(round(row1['Close'],2))
+            ticker_volume.append(round(row1['Volume'],2))
+    return render_template('index.html',ticker_list=ticker_list, ticker_date=ticker_date,ticker_open=ticker_open,ticker_high=ticker_high,ticker_close=ticker_close,ticker_volume=ticker_volume)
 
 @app.route('/insertintotable',methods = ['POST'])
 def insertintotable():
@@ -54,7 +88,7 @@ def insertintotable():
     #**************** FUNCTIONS TO FETCH DATA ***************************
     def get_historical(quote):
         end = datetime.now()
-        start = datetime(end.year-2,end.month,end.day)
+        start = datetime(end.year-5,end.month,end.day)
         data = yf.download(quote, start=start, end=end)
         df = pd.DataFrame(data=data)
         df.to_csv(''+quote+'.csv')
@@ -131,7 +165,10 @@ def insertintotable():
             print("Tomorrow's",quote," Closing Price Prediction by ARIMA Model :",arima_pred)
             #rmse calculation
             error_arima = math.sqrt(mean_squared_error(test, predictions))
+            
+            r2_arima = metrics.r2_score(test, predictions)
             print("ARIMA Mean Square Error:",error_arima)
+            print("ARIMA R- Square Error:",(round(r2_arima,2)*100))
             print("##############################################################################")
             return arima_pred, error_arima
         
@@ -252,7 +289,7 @@ def insertintotable():
         
         
         error_lstm = math.sqrt(mean_squared_error(real_stock_price, predicted_stock_price))
-        
+        r2_lstm = metrics.r2_score(real_stock_price, predicted_stock_price)
         
         #Forecasting Prediction
         forecasted_stock_price=regressor.predict(X_forecast)
@@ -265,6 +302,7 @@ def insertintotable():
         print("##############################################################################")
         print("Tomorrow's ",quote," Closing Price Prediction by LSTM Model: ",lstm_pred)
         print("LSTM Mean Square Error:",error_lstm)
+        print("LSTM R- Square Error:",(round(r2_lstm,2)*100))
         print("##############################################################################")
         return lstm_pred,error_lstm
     #***************** LINEAR REGRESSION SECTION ******************       
@@ -317,7 +355,7 @@ def insertintotable():
         plt2.close(fig)
         
         error_lr = math.sqrt(mean_squared_error(y_test, y_test_pred))
-        
+        r2_lr = metrics.r2_score(y_test, y_test_pred)
         
         #Forecasting
         forecast_set = clf.predict(X_to_be_forecasted)
@@ -328,6 +366,7 @@ def insertintotable():
         print("##############################################################################")
         print("Tomorrow's ",quote," Closing Price Prediction by Linear Regression Model: ",lr_pred)
         print("Linear Regression Mean Square Error:",error_lr)
+        print("LSTM R- Square Error:",(round(r2_lr,2)*100))
         print("##############################################################################")
         return df, lr_pred, forecast_set, mean, error_lr
     #**************** SENTIMENT ANALYSIS **************************
@@ -449,11 +488,178 @@ def insertintotable():
             print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
         return idea, decision
 
+    def WithtweetsDataset():
+        file_name = 'tweets.csv'
+        data = pd.read_csv(file_name, sep=';').set_index('id')
+        data.shape
+        print(data.head())
+        for i in range(5):
+            pprint(data.iat[i,1])
+        ticker_pattern = re.compile(r'(^\$[A-Z]+|^\$ES_F)')
+        ht_pattern = re.compile(r'#\w+')
 
+        ticker_dic = collections.defaultdict(int)
+        ht_dic = collections.defaultdict(int)
 
+        for text in data['text']:
+            for word in text.split():
+                if ticker_pattern.fullmatch(word) is not None:
+                    ticker_dic[word[1:]] += 1
+                
+                word = word.lower()
+                if ht_pattern.fullmatch(word) is not None:
+                    ht_dic[word] += 1
+        ticker_df = pd.DataFrame.from_dict(
+        ticker_dic, orient='index').rename(columns={0:'count'})\
+        .sort_values('count', ascending=False).head(20)
+    
+        ht_df = pd.DataFrame.from_dict(
+            ht_dic, orient='index').rename(columns={0:'count'})\
+            .sort_values('count', ascending=False).head(20)
 
+        fig, ax = plt.subplots(1, 2, figsize=(12,8))
+        plt.suptitle('Frequent Tickers and Hashtags', fontsize=16)
+        plt.subplots_adjust(wspace=0.4)
 
+        sns.barplot(x=ticker_df['count'], y=ticker_df.index, orient='h', ax=ax[0])
+        ax[0].set_title('Top 20 Tickers')
+
+        sns.barplot(x=ht_df['count'], y=ht_df.index, orient='h', ax=ax[1])
+        ax[1].set_title('Top 20 HashTags')
+
+        #plt.show()
+        charonly = re.compile(r'[^a-zA-Z\s]')
+        handle_pattern = re.compile(r'@\w+')
+        emoji_pattern = re.compile("["
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                "]+", flags=re.UNICODE)
+        url_pattern = re.compile(
+            'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+        pic_pattern = re.compile('pic\.twitter\.com/.{10}')
+        special_code = re.compile(r'(&amp;|&gt;|&lt;)')
+        tag_pattern = re.compile(r'<.*?>')
+
+        STOPWORDS = set(stopwords.words('english')).union(
+            {'rt', 'retweet', 'RT', 'Retweet', 'RETWEET'})
+
+        lemmatizer = WordNetLemmatizer()
+
+        def hashtag(phrase):
+            return ht_pattern.sub(' ', phrase)
+
+        def remove_ticker(phrase):
+            return ticker_pattern.sub('', phrase)
+            
+        def specialcode(phrase):
+            return special_code.sub(' ', phrase)
+
+        def emoji(phrase):
+            return emoji_pattern.sub(' ', phrase)
+
+        def url(phrase):
+            return url_pattern.sub('', phrase)
+
+        def pic(phrase):
+            return pic_pattern.sub('', phrase)
+
+        def html_tag(phrase):
+            return tag_pattern.sub(' ', phrase)
+
+        def handle(phrase):
+            return handle_pattern.sub('', phrase)
+
+        def decontracted(phrase):
+            # specific
+            phrase = re.sub(r"won\'t", "will not", phrase)
+            phrase = re.sub(r"can\'t", "can not", phrase)
+            
+            # DIS, ticker symbol of Disney, is interpreted as the plural of "DI" 
+            # in WordCloud, so I converted it to Disney
+            phrase = re.sub('DIS', 'Disney', phrase)
+
+            # general
+            phrase = re.sub(r"n\'t", " not", phrase)
+            phrase = re.sub(r"\'re", " are", phrase)
+            phrase = re.sub(r"(he|He)\'s", "he is", phrase)
+            phrase = re.sub(r"(she|She)\'s", "she is", phrase)
+            phrase = re.sub(r"(it|It)\'s", "it is", phrase)
+            phrase = re.sub(r"\'d", " would", phrase)
+            phrase = re.sub(r"\'ll", " will", phrase)
+            phrase = re.sub(r"\'t", " not", phrase)
+            phrase = re.sub(r"(\'ve|has)", " have", phrase)
+            phrase = re.sub(r"\'m", " am", phrase)
+            return phrase
+
+        def onlychar(phrase):
+            return charonly.sub('', phrase)
+
+        def remove_stopwords(phrase):
+            return " ".join([word for word in str(phrase).split()\
+                            if word not in STOPWORDS])
+
+        def tokenize_stem(phrase):   
+            tokens = word_tokenize(phrase)
+            stem_words =[]
+            for token in tokens:
+                word = lemmatizer.lemmatize(token)
+                stem_words.append(word)        
+            buf = ' '.join(stem_words)    
+            return buf
+        def arrange_text(ds):
+            ds['text2'] = ds['text'].apply(emoji)
+            ds['text2'] = ds['text2'].apply(handle)
+            ds['text2'] = ds['text2'].apply(specialcode)
+            ds['text2'] = ds['text2'].apply(hashtag)
+            ds['text2'] = ds['text2'].apply(url)
+            ds['text2'] = ds['text2'].apply(pic)
+            ds['text2'] = ds['text2'].apply(html_tag)
+            ds['text2'] = ds['text2'].apply(onlychar)
+            ds['text2'] = ds['text2'].apply(decontracted)
+            ds['text2'] = ds['text2'].apply(onlychar)
+            ds['text2'] = ds['text2'].apply(tokenize_stem)
+            ds['text2'] = ds['text2'].apply(remove_stopwords)
+        arrange_text(data)
+        for i in range(1000):
+            pprint(data.iat[i,3])
+            if data.iat[i,3].find(quote) > 0:
+                tw_list.append(data.iat[i,3])
+        
+        def wordcloud_by_sentiment(sentiment):
+            not_ticker = []
+
+            for text in data[data['sentiment']==sentiment]['text2']:
+                for word in text.split():
+                    if word.upper() not in ticker_dic:
+                        not_ticker.append(word.lower())
+            words = ' '.join([word for word in not_ticker])
+            return len(not_ticker)
+        pos = wordcloud_by_sentiment('positive')
+        neg = wordcloud_by_sentiment('negative')
+        neutral = wordcloud_by_sentiment('neutral')
+        print()
+        print("##############################################################################")
+        print("Positive Tweets :",pos,"Negative Tweets :",neg,"Neutral Tweets :",neutral)
+        print("##############################################################################")
+        labels=['Positive','Negative','Neutral']
+        sizes = [pos,neg,neutral]
+        explode = (0, 0, 0)
+        fig = plt.figure(figsize=(7.2,4.8),dpi=65)
+        fig1, ax1 = plt.subplots(figsize=(7.2,4.8),dpi=65)
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', startangle=90)
+        # Equal aspect ratio ensures that pie is drawn as a circle
+        ax1.axis('equal')  
+        plt.tight_layout()
+        plt.savefig('static/SA.png')
+        plt.close(fig)
+        
+        
     #**************GET DATA ***************************************
+    tw_list = []
     quote=nm
     #Try-except to check if valid stock symbol
     try:
@@ -463,6 +669,7 @@ def insertintotable():
     else:
     
         #************** PREPROCESSUNG ***********************
+        WithtweetsDataset()
         df = pd.read_csv(''+quote+'.csv')
         print("##############################################################################")
         print("Today's",quote,"Stock Data: ")
@@ -484,7 +691,7 @@ def insertintotable():
         df, lr_pred, forecast_set,mean,error_lr=LIN_REG_ALGO(df)
         #polarity,tw_list,tw_pol,pos,neg,neutral = retrieving_tweets_polarity(quote)
         polarity =0
-        tw_list = ''
+        #tw_list = ''
         tw_pol = ''
         #idea, decision=recommending(df, polarity,today_stock,mean)
         idea = ''
